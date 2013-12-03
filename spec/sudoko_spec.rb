9077952f719a0cfa388f02b8992aad7d6bd1f8ea
JamesGraham <jamesgraham09@gmail.com>
2013-12-03 12:41:03 +0000

    First commit

---
+++ spec/sudoko_spec.rb
@@ -0,0 +1,35 @@
+Require ./lib/grid
+
+
+describe Grid do
+  context "initialization" do
+    let(:puzzle) { '015003002000100906270068430490002017501040380003905000900081040860070025037204600' # it's an easy sudoku puzzle, row by row }
+    let(:grid) { Grid.new(puzzle) }
+    
+        it 'should have 81 cells' do
+            expect(grid.flatten.count).to eq(81)
+            end
+        
+        it 'should have an unsolved first cell' do
+            expect(grid[1]).to eq(0)
+            end
+        
+        it 'should have a solved second cell with value 1' do
+            expect(grid[2]).to eq(1)
+            end
+      
+  end
+end
+
+
+describe Grid do
+  let(:grid) { #init in here }
+  context "solving sudoku" do
+    it "can solve the puzzle" do
+      expect(grid.solved?).to be_false
+      grid.solve
+      expect(grid.solved?).to be_true
+      expect(grid.to_s).to eq('615493872348127956279568431496832517521746389783915264952681743864379125137254698')
+    end
+  end
+end
